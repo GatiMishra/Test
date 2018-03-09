12e7c401a1d96083548edd88e22c8d405cd26e9d
GatiMishra <mishrakgati@gmail.com>
2018-03-09 14:47:43 +0530

    testing after removal of static

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 from flask import Flask
 app = Flask(__name__)
 
-
-
-from app import routes
+@app.route('/')
+def hello_world():
+  return 'Hello, Jagadeesh!'
 
 if __name__ == '__main__':
   app.run()
