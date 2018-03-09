966b1c97c26064ca2e449ac16d63b2b1c18eea48
GatiMishra <mishrakgati@gmail.com>
2018-03-09 15:58:26 +0530

    Interim commit without database
    folders commited bootstrap,static,templates,featurerequestManager,featureRequestService and main.py

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
-from flask import Flask
+from flask import Flask,render_template
 app = Flask(__name__)
 
+@app.route('/FeatureRequestDetails')
+def featureRequestDetails():
+    """ featureRequestDetails function is used to display featureRequestDetails html page"""
+    return render_template('featureRequestDetails.html')
+
 @app.route('/')
-def hello_world():
-  return 'Hello, Jagadeesh!'
+def featureRequestForm():
+    """ featureRequestForm function is used to display featureRequestForm html page"""
+    return render_template('featureRequestForm.html')
 
 if __name__ == '__main__':
   app.run()
